f9a9d5abe46c5301e8825d76dd22f9672b21d706
Shagor Robidas <roysagor88@gmail.com>
2024-11-28 00:04:11 +0600

    last commit

--- Smart_Document/Smart_Document/views.py
+++ Smart_Document/Smart_Document/views.py
@@ -1,10 +1,26 @@
-from django.views.generic import TemplateView
+# https://docs.djangoproject.com/en/5.1/ref/contrib/messages/#using-messages-in-views-and-templates
+from django.shortcuts import redirect, render
+from django.views.generic import (
+    TemplateView,
+    FormView
+)
+from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginView(TemplateView):
     template_name = "login.html"
-# https://docs.djangoproject.com/en/5.1/ref/contrib/messages/#using-messages-in-views-and-templates
 
 
-class RegistrationView(TemplateView):
+class RegistrationView(FormView):
     template_name = "registration.html"
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+# view, view.grnatics, view.api view , view,viewset , modelviewset, query
